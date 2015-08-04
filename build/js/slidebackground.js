7b1ea5b024a69921b21e559b51773564dd6a8e1f
Bernardus Billy Tjiptoning <bernardus.billy@hotmail.com>
2015-08-04 05:41:51 -0700

    Update 1.0: Release

--- build/js/slidebackground.js
+++ build/js/slidebackground.js
@@ -1,8 +1,13 @@
-function slidebackground() {
-	console.log("slidebackground.js is ready.")
-	var bgs = $(".slidebackground").children();
-	$(bgs).each(function() {
-		var src = $(this).attr("data-trigger");
-		console.log("data-trigger");
-	});
-};
+$(window).ready(function() {
+	try {
+		console.log("slidebackground.js is ready.");
+		var bgs = $('.slidebackground').children();
+		$(bgs).each(function(index, content) {
+			var src = $(this).attr("src");
+			$(this).css("background-image", "url("+src+")");
+		});
+	}
+	catch(e) {
+		console.log(e);
+	}
+});
